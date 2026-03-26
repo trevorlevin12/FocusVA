1936e5373e2ad051937ea823a391ceac497f47b9
Jordan Levin <jordan@dionysustechnologygroup.com>
2026-03-25 23:20:01 -0700

    feat: swap service-account config for OAuth vars

--- config.py
+++ config.py
@@ -4,8 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 ANTHROPIC_API_KEY: str = os.getenv("ANTHROPIC_API_KEY", "")
+OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY", "")
 GMAIL_CREDENTIALS_PATH: str = os.getenv("GMAIL_CREDENTIALS_PATH", "")
 GMAIL_TOKEN_PATH: str = os.getenv("GMAIL_TOKEN_PATH", "./token.json")
+GMAIL_REDIRECT_URI: str = os.getenv("GMAIL_REDIRECT_URI", "http://localhost:8000/auth/callback")
 TARGET_EMAIL: str = os.getenv("TARGET_EMAIL", "inbox@focusgraphics.com")
 POLL_INTERVAL_SECONDS: int = int(os.getenv("POLL_INTERVAL_SECONDS", "120"))
 DB_PATH: str = os.getenv("DB_PATH", "./focusva.db")
